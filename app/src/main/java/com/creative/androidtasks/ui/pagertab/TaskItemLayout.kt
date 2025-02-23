cdbacbcd62d313197f15b526ef32af8faa127b57
dantech0xff <danhtran.dev@outlook.com>
2025-02-24 00:11:51 +0700

    add ui completed

--- app/src/main/java/com/creative/androidtasks/ui/pagertab/TaskItemLayout.kt
+++ app/src/main/java/com/creative/androidtasks/ui/pagertab/TaskItemLayout.kt
@@ -2,15 +2,18 @@ package com.creative.androidtasks.ui.pagertab
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.creative.androidtasks.TaskDelegate
@@ -37,13 +40,25 @@ fun TaskItemLayout(
                 taskDelegate.invertTaskCompleted(state)
             }
         )
-        Text(
-            text = state.content,
-            modifier = Modifier.weight(1.0f).padding(horizontal = 4.dp)
-        )
-        Text(if (state.isFavorite) "👍" else "👎", modifier = Modifier.padding(6.dp).clickable {
-            taskDelegate.invertTaskFavorite(state)
-        })
+        Column(
+            modifier = Modifier.weight(1.0f).wrapContentHeight(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.Start
+        ) {
+            Text(
+                text = state.content,
+                modifier = Modifier.padding(horizontal = 4.dp),
+                textDecoration = TextDecoration.LineThrough.takeIf { state.isCompleted }
+            )
+            if (state.isCompleted) {
+                Text(text = "Completed: ${state.updatedAt}", modifier = Modifier.padding(horizontal = 4.dp),)
+            }
+        }
+        if (!state.isCompleted) {
+            Text(if (state.isFavorite) "👍" else "👎", modifier = Modifier.padding(6.dp).clickable {
+                taskDelegate.invertTaskFavorite(state)
+            })
+        }
     }
 }
 
